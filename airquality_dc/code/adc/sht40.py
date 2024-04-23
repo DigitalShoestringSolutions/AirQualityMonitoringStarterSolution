e152100e534ae8f3edf1a927f29b5fac105568a3
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-04-23 13:58:15 +0100

    Update sht40.py
    Separated public and private functions

--- airquality_dc/code/adc/sht40.py
+++ airquality_dc/code/adc/sht40.py
@@ -6,29 +6,38 @@
 # imports
 from smbus2 import SMBus
 
-SHT40_I2C_ADDRESS = 0x44
-COMMAND_READ_TRH = 0xFD
+_SHT40_I2C_ADDRESS = 0x44
+_COMMAND_READ_TRH = 0xFD
 
-def read():
+def _read():
+  """Read bytes containing temperature and humidity data from the i2c bus"""
   with SMBus(1) as bus:
-    read_bytes = bus.read_i2c_block_data(SHT40_I2C_ADDRESS, COMMAND_READ_TRH, 6)
+    read_bytes = bus.read_i2c_block_data(_SHT40_I2C_ADDRESS, _COMMAND_READ_TRH, 6)
     S_T = read_bytes[0:1]
     S_RH = read_bytes[3:4]
   return S_T, S_RH
 
-def calculate_temperature(S_T):
+def _calculate_temperature(S_T):
+  """Calculate a temperature from adc bytes"""
   T_degC = -45 + (175*S_T/65535)
   return T_degC
 
-def calculate_relativehumidity(S_RH):
+def _calculate_relativehumidity(S_RH):
+  """Calculate a relative humidity from adc bytes"""
   RH = -6 + (125*S_RH/65535)
   return RH
+
+def get_TRH():
+  readings = _read()
+  T = _calculate_temperature(readings[0])       # degC
+  RH = _calculate_relativehumidity(readings[1]) # %
+  return T, RH
 
 if __name__ == '__main__':
   import time
   while True:
-    readings = read()
-    print("T:", calculate_temperature(readings[0]), "degC")
-    print("RH:", calculate_relativehumidity(readings[1]), "%")
+    TRH = get_TRH()
+    print("T:", TRH[0], "degC")
+    print("RH:", TRH[1], "%")
     print()
     time.sleep(1)
