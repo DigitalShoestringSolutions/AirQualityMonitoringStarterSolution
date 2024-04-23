b4ef32e41e832862f18615f3ab6d4809bd47bf76
tobyaharris <51968582+tobyaharris@users.noreply.github.com>
2024-04-23 14:13:30 +0100

    Create sht40bmp280.py
    Wrapper for using both sensors together

---
+++ airquality_dc/code/adc/sht40bmp280.py
@@ -0,0 +1,36 @@
+# sht40bmp280.py
+
+# Wrapper for using the M5STACK ENV IV environmental sensor
+
+# local imports
+import sht40
+import bmp280_wrapper
+
+
+class ADC: # as required by measure.py
+
+  def sample():
+    """Sample both the sht40 and bmp280"""
+
+    Ts, RH = sht40.get_TRH()
+    Tb, P = bmp280_wrapper.get_TP()
+    
+    data = Data()    
+    data.temperature = Ts
+    data.humidity = RH
+    data.pressure = P
+    return data
+
+  class Data:
+    pass
+
+if __name__ == '__main__':
+  import time
+  sensor = ADC()
+  while True:
+    sampledata = sensor.sample()
+    print("T:", sampledata.temperature, "degC")
+    print("RH:", sampledata.humidity, "%")
+    print("P:", sampledata.pressure, "%")
+    print()
+    time.sleep(1)
